a4c29d7f92b47574e63fa799d04583d296a79a9e
Soanvig <soanvig@gmail.com>
2022-11-07 19:56:41 +0100

    chore: format with rustfmt

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -7,11 +7,11 @@ mod text;
 mod url;
 
 pub trait Interpreter {
-    fn check(&self, input: &str) -> bool;
-    fn interpet(&self, input: &str) -> Bookmark;
-    fn belongs(&self, interpreted_by: &str) -> bool;
+  fn check(&self, input: &str) -> bool;
+  fn interpet(&self, input: &str) -> Bookmark;
+  fn belongs(&self, interpreted_by: &str) -> bool;
 }
 
 pub fn get_interpreters() -> Vec<Box<dyn Interpreter>> {
-    vec![Box::new(TextInterpreter {})]
+  vec![Box::new(TextInterpreter {})]
 }
