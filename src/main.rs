78fca10aae4e22e13cf164bc8f6f0685508f58ad
Soanvig <soanvig@gmail.com>
2022-11-07 21:55:02 +0100

    refactor: chain main logic

--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,21 @@ fn main() {
   let environment = Environment::new();
   let clipboard = environment.operating_system.get_clipboard();
 
-  match clipboard {
-    Some(clipboard) => {
+  clipboard
+    .and_then(|clipboard| {
       println!("Clipboard content: {:?}", clipboard);
 
-      let interpreter = environment
+      environment
         .interpreters
         .iter()
-        .find(|i| i.check(&clipboard));
+        .find(|i| i.check(&clipboard))
+        .map(|interpreter| (clipboard, interpreter))
+    })
+    .and_then(|(clipboard, interpreter)| {
+      let bookmark = interpreter.interpet(&clipboard);
 
-      match interpreter {
-        Some(interpreter) => {
-          let bookmark = interpreter.interpet(&clipboard);
+      environment.storage.store_bookmark(&environment, &bookmark);
 
-          environment.storage.store_bookmark(&environment, &bookmark);
-        }
-        None => {
-          println!("No matching interpreter found for \"{}\"", clipboard);
-        }
-      }
-    }
-    None => {}
-  }
+      Some(())
+    });
 }
